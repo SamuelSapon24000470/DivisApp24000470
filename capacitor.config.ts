4feef74c6117e91617fbf99d895e1cb46012bbe8
SamuelSapon24000470 <159086064+SamuelSapon24000470@users.noreply.github.com>
2025-11-23 13:40:59 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DivisApp24000470',
+  webDir: 'dist'
+};
+
+export default config;
